ba50b05054e3c2797fe31d5bbea84e485124ed37
Narendra Agrawal <nagrawal@maprtech.com>
2014-07-11 23:38:14 -0700

    MAPR-13006: Bulk Load support for MapR tables
     - In configureForIncrementalLoad(), remember the table path in job conf
     - Added another api confifureMapRTablePath() for users who want to setup a  custom partitioner
     - return BulkLoadRecordWriter if MapR table
    
    #Squashed: MAPR-13164: Add bulkload option for M7 table create/alter in HBase shell.
    #Squashed: MAPR-14571: use com.mapr.fs.hbase for mapr specific implementation.
    #Squashed: MAPR-14852: configure serialization class for Result.
    
    Conflicts:
    
    	hbase-server/src/main/java/org/apache/hadoop/hbase/mapreduce/HFileOutputFormat2.java

--- hbase-shell/src/main/ruby/hbase.rb
+++ hbase-shell/src/main/ruby/hbase.rb
@@ -81,6 +81,7 @@ module HBaseConstants
   #MapR constants
   PERM = "PERM"
   EXPR = "EXPR"
+  BULKLOAD = "BULKLOAD"
 
   # Load constants from hbase java API
   def self.promote_constants(constants)
